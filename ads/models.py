f8120362e5899a2d9df93afb6e091c0fec9468a2
Tereskina <iv.kseni2@gmail.com>
2023-03-05 22:41:24 +0000

    Add user. Alter ads/models.py

--- ads/models.py
+++ ads/models.py
@@ -1,22 +1,38 @@
 from django.db import models
 
+from users.models import User
+
+
+class Category(models.Model):
+
+    # slug = models.SlugField(max_length=50)
+    name = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
 
 class Ads(models.Model):
-    slug = models.SlugField(max_length=50)
-    name = models.CharField(max_length=100)
-    author = models.CharField(max_length=20)
+    # slug = models.SlugField(max_length=50)
+    name = models.CharField(max_length=100, unique=True)
+    author = models.ForeignKey(User, verbose_name='Автор', related_name='ads', on_delete=models.CASCADE)
     price = models.PositiveIntegerField()
     description = models.TextField(max_length=2000, null=True)
-    address = models.CharField(max_length=100)
-    is_published = models.BooleanField(default=False)
+    is_published = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    # address = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='pictures', null=True, blank=True)
 
-    # def __str__(self):
-    #     return self.name
+    class Meta:
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
+
+    def __str__(self):
+        return self.name
 
 
-class Categories(models.Model):
-    slug = models.SlugField(max_length=50)
-    name = models.SlugField(max_length=100)
-    #
-    # def __str__(self):
-    #     return self.name
+
--- django_27_HW/settings.py
+++ django_27_HW/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,13 +75,22 @@ WSGI_APPLICATION = 'django_27_HW.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -123,3 +132,31 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Pagination
+
+TOTAL_ON_PAGE = 10
+
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         'console': {
+#             'level': 'DEBUG',
+#             'class': 'logging.StreamHandler'
+#         },
+#     },
+#     'loggers': {
+#         '': {
+#             'handlers': ['console'],
+#             'level': 'DEBUG',
+#             'propagate': False,
+#         },
+#         'django.db': {
+#             'level': 'DEBUG'
+#         },
+#     },
+# }
