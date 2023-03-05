8f2059b2f726b28beabaca440818a97422f0b6c7
Tereskina <iv.kseni2@gmail.com>
2023-03-05 21:56:56 +0000

    change DATABASES in settings.py

--- django_27_HW/settings.py
+++ django_27_HW/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'django_27_HW.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
